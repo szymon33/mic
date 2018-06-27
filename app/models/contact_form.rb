6ce7c444f3a3537e9702465320ab83e2da7c45c0
Szymon Miedziejko <miedziejko@gmail.com>
2018-06-27 16:30:25 +0100

    Add swagger API validator to ContactForm model

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -15,14 +15,28 @@ class ContactForm
   attr_reader :message
 
   validates :name, :business_name, :telephone_number, :email, presence: true
+  validate :swagger_api_validator_and_storage
 
   def save
     if valid?
       @message = 'Enqueue success'
       true
     else
-      @message = 'Format errors on validation'
+      @message ||= 'Format errors on validation'
       false
+    end
+  end
+
+  protected
+
+  def swagger_api_validator_and_storage
+    result = SwaggerClient.new.create(as_json)
+    if result
+      result['errors'].each { |msg| errors[:base] << msg }
+      @message = result['message']
+    else
+      errors[:base] << 'Server is not responding'
+      @message = 'Server is not responding'
     end
   end
 end
--- spec/models/contact_form_spec.rb
+++ spec/models/contact_form_spec.rb
@@ -1,17 +1,24 @@
 require 'rails_helper'
 
 describe ContactForm do
-  it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_presence_of(:business_name) }
-  it { is_expected.to validate_presence_of(:telephone_number) }
-  it { is_expected.to validate_presence_of(:email) }
-  it { is_expected.to respond_to(:contact_time) }
-  it { is_expected.to respond_to(:notes) }
-  it { is_expected.to respond_to(:reference) }
-
   let(:contact) { build(:contact_form) }
+  let(:stub_swagger_api) { allow_any_instance_of(SwaggerClient).to receive(:create) { { 'errors' => [] } } }
 
-  describe '.save' do
+  describe 'when initialised' do
+    before { stub_swagger_api }
+
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:business_name) }
+    it { is_expected.to validate_presence_of(:telephone_number) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to respond_to(:contact_time) }
+    it { is_expected.to respond_to(:notes) }
+    it { is_expected.to respond_to(:reference) }
+  end
+
+  describe '.save', vcr: false do
+    before { stub_swagger_api }
+
     it 'is truthy' do
       expect(contact.save).to be_truthy
     end
@@ -22,8 +29,10 @@ describe ContactForm do
     end
   end
 
-  describe 'message attribute' do
+  describe 'message attribute', vcr: false do
     let(:subject) { contact.message }
+
+    before { stub_swagger_api }
 
     it { is_expected.to be_nil }
 
@@ -38,6 +47,34 @@ describe ContactForm do
       expect { contact.save }.to change {
         contact.message
       }.to 'Format errors on validation'
+    end
+  end
+
+  describe 'swagger_api_validator_and_storage' do
+    it 'calls SwaggerClient' do
+      expect_any_instance_of(SwaggerClient).to receive(:create)
+      contact.save
+    end
+
+    it 'sets server not responding when no result' do
+      allow_any_instance_of(SwaggerClient).to receive(:create)
+
+      expect { contact.save }.to change {
+        contact.errors.full_messages
+      }.to(['Server is not responding'])
+    end
+
+    it 'sets no errors when no errors' do
+      stub_swagger_api
+      contact.save
+      expect(contact.errors).to be_empty
+    end
+
+    it 'sets spefic error' do
+      allow_any_instance_of(SwaggerClient).to receive(:create) { { 'errors' => ['La La Land'] } }
+      expect { contact.save }.to change {
+        contact.errors.full_messages
+      }.to(['La La Land'])
     end
   end
 end
