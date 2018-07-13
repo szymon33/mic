6ff0570fb3d3d73afe38e2364e82c6a5ca82ff79
Szymon <szymon@Szymons-MacBook-Pro.local>
2018-07-13 14:54:02 +0100

    Add simple test to ContactFormCtrl

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -15,8 +15,13 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      './src/js/**/*.test.js'
+      "./node_modules/angular/angular.min.js",
+      "./node_modules/angular-mocks/angular-mocks.js",
+      "./node_modules/angular-route/angular-route.js",
+      "./node_modules/angular-resource/angular-resource.js",
+      "./src/js/**/*.js"
     ],
+
 
     // list of files / patterns to exclude
     exclude: [
--- frontend/src/js/controllers/contactFormCtrl.test.js
+++ frontend/src/js/controllers/contactFormCtrl.test.js
@@ -1,5 +1,20 @@
-describe('playground', function () {
-  it('true', function () {
-    expect(true).toBe(true);
-  }); 
+describe('ContactFormCtrl', function() {
+  beforeEach(module('micApp'));
+
+  var controller, ContactFormsResource, $scope, $http;
+
+  beforeEach(inject(function($rootScope, $http, $controller){
+    ContactFormsResource = {
+      save: function() {}
+    };
+
+    spyOn(ContactFormsResource, 'save').and.returnValue("Foo");
+    $scope = $rootScope.$new();
+
+    controller = $controller('ContactFormCtrl', { $scope: $scope, $http: $http, ContactFormsResource: ContactFormsResource });
+  }));
+
+  it('should have correct name', function() {
+    expect($scope.name).toEqual('ContactFormCtrl');
+  });
 });
